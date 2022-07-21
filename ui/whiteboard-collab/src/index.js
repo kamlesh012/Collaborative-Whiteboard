635acdeff032aba6e08cd5bc3454c010f342ee94
Kamlesh Bisht <75537446+kamlesh012@users.noreply.github.com>
2022-07-21 17:56:26 +0530

    first commit

---
+++ ui/whiteboard-collab/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Container from './components/container/Container';
+
+ReactDOM.render(
+  <React.StrictMode>
+
+    <Container/>
+    
+  </React.StrictMode>,
+  document.getElementById('root')
+);
